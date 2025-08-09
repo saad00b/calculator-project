68fbd83dbd095aed3e4a3bc812cc97268538ecd3
saad00b <saad.bencharra@gmail.com>
2025-08-09 10:49:25 +0100

    Make operate function that does all operations

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function add(a,b) {return a+b;}
 function subtract(a,b) {return a-b;}
 function multiply(a,b) {return a*b;}
 function divide(a,b) {return a/b;}
+
+function operate(a,b,op) {
+    if (op === "+") {add(a,b);}
+    if (op === "-") {subtract(a,b);}
+    if (op === "*") {multiply(a,b);}
+    if (op === "/") {divide(a,b);}
+}
+
+let num1 ;
+let num2 ;
+let operator ;
