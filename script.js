239310eb4bed1be8c9dbd0664b33c61471f1e688
saad00b <saad.bencharra@gmail.com>
2025-08-11 10:55:23 +0100

    Do basic logic for operations and their display

--- script.js
+++ script.js
@@ -4,32 +4,47 @@ function multiply(a,b) {return a*b;}
 function divide(a,b) {return a/b;}
 
 function operate(a,b,op) {
-    if (op === "+") {add(a,b);}
-    if (op === "-") {subtract(a,b);}
-    if (op === "*") {multiply(a,b);}
-    if (op === "/") {divide(a,b);}
+    if (op === "+") {return add(a,b);}
+    if (op === "-") {return subtract(a,b);}
+    if (op === "*") {return multiply(a,b);}
+    if (op === "/") {return divide(a,b);}
 }
 
 let activeNumber ="";
-let num1 ;
-let num2 ;
+let nums = [] ;
+let result ;
 let operator ;
+let display = document.querySelector(".display");
+display.textContent = "";
+
 
 let numbers = Array.from(document.querySelectorAll(".digit"));
 for (num of numbers) {
     num.addEventListener("click",(e) => {
         activeNumber += e.target.textContent;
-        console.log(activeNumber);});
+        console.log(activeNumber);
+        display.textContent += e.target.textContent;
+    });
 }
 
 let operators = Array.from(document.querySelectorAll(".operator"));
 for (op of operators) {
     op.addEventListener("click", (e) => {
-        operator = e.target.textContent
+        operator = e.target.textContent ;
         console.log(operator);
-        num1 = parseInt(activeNumber);
-        console.log(`number 1 is ${num1}`);
+        nums.push(parseInt(activeNumber));
+        console.log(`number 1 is ${nums[0]}`);
         activeNumber = "";
+        display.textContent += operator; 
     });
 }
+
+let resultButton = document.querySelector("#result");
+resultButton.addEventListener("click",() => {
+    nums.push(parseInt(activeNumber));
+    if (nums.length === 2) {
+        result = operate(nums[0],nums[1],operator);
+        display.textContent = `${result}` ;
+    }
+})
 
